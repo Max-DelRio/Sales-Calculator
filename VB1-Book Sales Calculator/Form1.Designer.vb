59e4efd7c38606803918e3909b86e9e4991bf6c2
Max Del Rio <34142352+SilentHylian@users.noreply.github.com>
2017-11-30 16:03:17 -0800

    Sales Calculator

---
+++ VB1-Book Sales Calculator/Form1.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.facePictureBox = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.salesTextBox = New System.Windows.Forms.TextBox()
+        Me.codeComboBox = New System.Windows.Forms.ComboBox()
+        Me.percentLabel = New System.Windows.Forms.Label()
+        Me.discountLabel = New System.Windows.Forms.Label()
+        Me.salesTaxLabel = New System.Windows.Forms.Label()
+        Me.totalDueLabel = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.calculateButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.facePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'facePictureBox
+        '
+        Me.facePictureBox.Image = CType(resources.GetObject("facePictureBox.Image"), System.Drawing.Image)
+        Me.facePictureBox.Location = New System.Drawing.Point(8, 8)
+        Me.facePictureBox.Name = "facePictureBox"
+        Me.facePictureBox.Size = New System.Drawing.Size(50, 50)
+        Me.facePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.facePictureBox.TabIndex = 0
+        Me.facePictureBox.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Arial", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(80, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(280, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Book Sales"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'salesTextBox
+        '
+        Me.salesTextBox.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.salesTextBox.Location = New System.Drawing.Point(72, 96)
+        Me.salesTextBox.Name = "salesTextBox"
+        Me.salesTextBox.Size = New System.Drawing.Size(80, 22)
+        Me.salesTextBox.TabIndex = 2
+        Me.salesTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'codeComboBox
+        '
+        Me.codeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.codeComboBox.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.codeComboBox.FormattingEnabled = True
+        Me.codeComboBox.Items.AddRange(New Object() {"None", "Student", "Faculty", "Staff", "Special"})
+        Me.codeComboBox.Location = New System.Drawing.Point(176, 96)
+        Me.codeComboBox.Name = "codeComboBox"
+        Me.codeComboBox.Size = New System.Drawing.Size(80, 24)
+        Me.codeComboBox.TabIndex = 3
+        '
+        'percentLabel
+        '
+        Me.percentLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.percentLabel.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.percentLabel.Location = New System.Drawing.Point(280, 96)
+        Me.percentLabel.Name = "percentLabel"
+        Me.percentLabel.Size = New System.Drawing.Size(80, 20)
+        Me.percentLabel.TabIndex = 4
+        Me.percentLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'discountLabel
+        '
+        Me.discountLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.discountLabel.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.discountLabel.Location = New System.Drawing.Point(72, 160)
+        Me.discountLabel.Name = "discountLabel"
+        Me.discountLabel.Size = New System.Drawing.Size(80, 20)
+        Me.discountLabel.TabIndex = 5
+        Me.discountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'salesTaxLabel
+        '
+        Me.salesTaxLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.salesTaxLabel.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.salesTaxLabel.Location = New System.Drawing.Point(176, 160)
+        Me.salesTaxLabel.Name = "salesTaxLabel"
+        Me.salesTaxLabel.Size = New System.Drawing.Size(80, 20)
+        Me.salesTaxLabel.TabIndex = 6
+        Me.salesTaxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'totalDueLabel
+        '
+        Me.totalDueLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.totalDueLabel.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.totalDueLabel.Location = New System.Drawing.Point(280, 160)
+        Me.totalDueLabel.Name = "totalDueLabel"
+        Me.totalDueLabel.Size = New System.Drawing.Size(80, 20)
+        Me.totalDueLabel.TabIndex = 7
+        Me.totalDueLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(72, 72)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(80, 23)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Sales"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(176, 72)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(80, 23)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Code"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(280, 72)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(80, 23)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "Percent"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label9
+        '
+        Me.Label9.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(72, 137)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(80, 23)
+        Me.Label9.TabIndex = 11
+        Me.Label9.Text = "Discount"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(176, 137)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(80, 23)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "Sales Tax"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label11
+        '
+        Me.Label11.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(280, 137)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(80, 23)
+        Me.Label11.TabIndex = 13
+        Me.Label11.Text = "Total Due"
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'clearButton
+        '
+        Me.clearButton.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearButton.Location = New System.Drawing.Point(72, 216)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(80, 23)
+        Me.clearButton.TabIndex = 14
+        Me.clearButton.Text = "C&lear"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'calculateButton
+        '
+        Me.calculateButton.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.calculateButton.Location = New System.Drawing.Point(176, 216)
+        Me.calculateButton.Name = "calculateButton"
+        Me.calculateButton.Size = New System.Drawing.Size(80, 23)
+        Me.calculateButton.TabIndex = 15
+        Me.calculateButton.Text = "Ca&lculate"
+        Me.calculateButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitButton.Location = New System.Drawing.Point(280, 216)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(80, 23)
+        Me.exitButton.TabIndex = 16
+        Me.exitButton.Text = "E&xit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 256)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(158, 16)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Created by Max Del Rio"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(392, 281)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.calculateButton)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.totalDueLabel)
+        Me.Controls.Add(Me.salesTaxLabel)
+        Me.Controls.Add(Me.discountLabel)
+        Me.Controls.Add(Me.percentLabel)
+        Me.Controls.Add(Me.codeComboBox)
+        Me.Controls.Add(Me.salesTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.facePictureBox)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Book Sales Calculator"
+        CType(Me.facePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents facePictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents salesTextBox As TextBox
+    Friend WithEvents codeComboBox As ComboBox
+    Friend WithEvents percentLabel As Label
+    Friend WithEvents discountLabel As Label
+    Friend WithEvents salesTaxLabel As Label
+    Friend WithEvents totalDueLabel As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents clearButton As Button
+    Friend WithEvents calculateButton As Button
+    Friend WithEvents exitButton As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ VB1-Book Sales Calculator/Form1.vb
@@ -0,0 +1,185 @@
+﻿' ******************************************************************************
+' *                               VB2-Calculator                               *
+' ******************************************************************************
+' *                                                                            *
+' *  Programmer   : Max  Del RIo                                               *
+' *  Chapter      : VB2                                                        *
+' *  Assignment   : ASSN#2                                                     *
+' *  Form Name    : VB2-Calculator                                             *
+' *  Date Created : 11/10/2016                                                 *
+' *  Description  : This form calculates a sales discount, sales tax, and      *
+' *                 total due.                                                 *
+' *                                                                            *
+' ******************************************************************************
+
+' Set Define Variables Option On
+Option Explicit On
+
+' Set No Automatic Data Conversion On
+Option Strict On
+
+
+Public Class Form1
+    ' Set Sales Limit constant = $500
+    Const SALES_LIMIT_Integer As Integer = 500
+
+    ' Set Double Discount Limit constant = $300
+    Const DOUBLE_DISCOUNT_LIMIT_Integer As Integer = 300
+
+    ' Sete Sales Tax Percent constant = 8.75%
+    Const SALES_TAX_PERCENT_Decimal As Decimal = 0.0875D
+
+    ' Set Minimum Discount Percent constant = 10%
+    Const MINIMUM_DISCOUNT_PERCENT_Decimal As Decimal = 0.1D
+
+    ' Set Maximum Discount Percent constant = 15%
+    Const MAXIMUM_DISCOUNT_PERCENT_Decimal As Decimal = 0.15D
+
+    ' Set Special Discount Percent constant = 20%
+    Const SPECIAL_DISCOUNT_PERCENT_Decimal As Decimal = 0.2D
+
+    Private Sub calculateButton_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+        ' **************************************************************************
+        ' *  Procedure    : Calculate-Click                                        *
+        ' *  Description  : The Calculate button determines, calculates, and       *
+        ' *                 displays the Percent, Discount, Sales Tax, and Total   *
+        ' *                 Due. It displays a Face image for supervisor approval  *
+        ' *                 for Sales greater than or equal to the Sales Limit.    *
+        ' **************************************************************************
+
+        ' Define Variables: Sales, Code, Percent, Discount, Sales Tax, Total Due
+        Dim salesDecimal, percentDecimal, discountDecimal, salesTaxDecimal, totalDueDecimal As Decimal
+        Dim codeString As String
+
+        ' Get Sales from Form
+        salesDecimal = Decimal.Parse(salesTextBox.Text)
+
+        ' Get Code from Form
+        codeString = codeComboBox.Text
+
+        ' IF Code = "Student"
+        If codeString = "Student" Then
+
+            ' Percent = Minimum Discount Percent constant
+            percentDecimal = MINIMUM_DISCOUNT_PERCENT_Decimal
+
+            ' Else IF Code = "Falculty" Or Code = "Staff"
+        ElseIf codeString = "Faculty" Or codeString = "Staff" Then
+
+            ' Percent = Maximum Discount Percent constant
+            percentDecimal = MAXIMUM_DISCOUNT_PERCENT_Decimal
+
+            ' Else IF Code = "Special"
+        ElseIf codeString = "Special" Then
+
+            ' Percent = Special Discount Percent constant
+            percentDecimal = SPECIAL_DISCOUNT_PERCENT_Decimal
+
+            ' Else
+        Else
+
+            ' Percent = 0
+            percentDecimal = 0
+
+            ' End IF
+        End If
+
+        ' IF Sales <=Double Discount Limit constant
+        If salesDecimal <= DOUBLE_DISCOUNT_LIMIT_Integer Then
+
+            ' Calculate Discount = Sales * Percent
+            discountDecimal = salesDecimal * percentDecimal
+
+            ' Else
+        Else
+
+            ' Calculate Discount = Double Discount Limit constant * Percent + (Sales - Double Discount Limit constant) * (2 * Percent)
+            discountDecimal = DOUBLE_DISCOUNT_LIMIT_Integer * percentDecimal + (salesDecimal - DOUBLE_DISCOUNT_LIMIT_Integer) * (2D * percentDecimal)
+
+            ' End IF
+        End If
+
+        ' Calculate Sales Tax = (Sales - Discount) * Sales Tax Percent constant
+        salesTaxDecimal = (salesDecimal - discountDecimal) * SALES_TAX_PERCENT_Decimal
+
+        ' Calculate Total Due = Sales - Discount + Sales Tax
+        totalDueDecimal = salesDecimal - discountDecimal + salesTaxDecimal
+
+        ' Display Percent
+        percentLabel.Text = percentDecimal.ToString("P2")
+
+        ' Display Discount
+        discountLabel.Text = discountDecimal.ToString("C2")
+
+        ' Display Sales Tax
+        salesTaxLabel.Text = salesTaxDecimal.ToString("C2")
+
+        ' Display Total Due
+        totalDueLabel.Text = totalDueDecimal.ToString("C2")
+
+        ' IF Sales >= Sales Limit constant
+        If salesDecimal >= SALES_LIMIT_Integer Then
+
+            ' Display Face Image
+            facePictureBox.Visible = True
+
+            ' Else
+        Else
+
+            ' Hide Face Image
+            facePictureBox.Visible = False
+            ' End IF
+        End If
+
+        ' End Calculate-Click
+    End Sub
+
+    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        ' **************************************************************************
+        ' *  Procedure    : Clear-Click                                            *
+        ' *  Description  : The Clear button clears the Sales, Percent, Discount,  *
+        ' *                 Sales Tax, and Total Due information. It sets the Code *
+        ' *                 to "None", makes the Face image invisible, and sets    *
+        ' *                 the focus to the sales.                                *
+        ' **************************************************************************
+
+        ' Define Variables: None
+
+        ' Clear Sales TextBox
+        salesTaxLabel.Text = ""
+
+        ' Set Code ComboBox to "None"
+        codeComboBox.Text = "None"
+
+        ' Clear Percent Label
+        percentLabel.Text = ""
+
+        ' Clear Discount Label
+        discountLabel.Text = ""
+
+        ' Clear Sales Tax Label
+        salesTaxLabel.Text = ""
+
+        ' Clear Total Due Label
+        totalDueLabel.Text = ""
+
+        ' Make Face Image invisible
+        facePictureBox.Visible = False
+
+        ' Set the Focus to Sales TextBox
+        salesTextBox.Focus()
+
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        ' **************************************************************************
+        ' *  Procedure    : Exit-Click                                             *
+        ' *  Description  : When the Exit Button is clicked the form is closed.    *
+        ' **************************************************************************
+
+        ' Define Variables: None
+        ' Close the Form
+        Me.Close()
+
+    End Sub
+End Class
